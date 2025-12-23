54c45fc4ba57be5a2f8a63295dd1650e896a9f96
Mohammad Yunus <zishaanyunus@gmail.com>
2025-12-23 12:06:57 +0400

    Add basic Amazon Clone header

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+    <div className="App">
+      <header className="header">
+        <h1>Amazon Clone</h1>
+      </header>
+      <main>
+        <p>Welcome, Mohammad Yunus! Your Amazon-like store UI will appear here.</p>
+      </main>
+    </div>
+  );
+
+}
+
+export default App;
